178b159bd04ed92574aa0e097dff74f7bad203aa
mikaaasaa <mikasaackerman102001@gmail.com>
2026-02-16 15:49:38 +0530

    github action configured

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Set the base path for GitHub Pages.
+  // If you are deploying to https://<USERNAME>.github.io/<REPO>/, set base to '/<REPO>/'.
+  // If you are deploying to https://<USERNAME>.github.io/, set base to '/'.
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
